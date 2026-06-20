8cd87c6fa346927fdd060e6e41f7c76830c8a837
Yuriy-vasylevsky <yuriy.vasylevsky@gmail.com>
2026-06-20 20:44:10 +0300

    2

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 # ── API URL твого ngrok/бота ──
 NGROK_API = os.environ.get(
     "API_URL",
-    "static.244.71.42.77.clients.your-server.de/api/safe"
+    "static.244.71.42.77.clients.your-server.de"
+    #  "static.244.71.42.77.clients.your-server.de/api/safe"
 )
 
 @app.route('/')
